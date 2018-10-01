5b077636d48564f58350500454f3246730da061e
madhusudhan <madhusudhan.sahni@in.accemy.com>
2018-10-01 17:42:35 +0530

    network listner added

---
+++ mydatasavelibrary/src/main/java/com/example/mydatasavelibrary/NetworkChangeReceiver.java
@@ -0,0 +1,40 @@
+package com.example.mydatasavelibrary;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+
+public class NetworkChangeReceiver extends BroadcastReceiver
+{
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        try
+        {
+            if (isOnline(context)) {
+                Toast.makeText(context, "Online Connect Intenet", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context, " not Connect Intenet", Toast.LENGTH_SHORT).show();
+
+            }
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isOnline(Context context) {
+        try {
+            ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo netInfo = cm.getActiveNetworkInfo();
+            //should check null because in airplane mode it will be null
+            return (netInfo != null && netInfo.isConnected());
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
